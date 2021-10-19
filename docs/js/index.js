ee4d3cc672a2bbf087e5c808c8700c7a2ccda358
AnaBergallo <ana.bergallo@correo.ucu.edu.uy>
2021-10-18 23:16:00 -0300

    fix modal

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,13 +1,20 @@
 const btn_open = document.getElementById('btn_open');
 const modal_container = document.getElementById('modal_container');
 const btn_close = document.getElementById('btn_close');
+const form = document.getElementById('form');
 
 btn_open.addEventListener('click', showModal);
 btn_close.addEventListener('click', hideModal);
+
+form.addEventListener('submit', fixFormulario)
 
 function showModal() {
     modal_container.classList.add('show');
 }
 function hideModal() {
+    form.reset();
     modal_container.classList.remove('show');
+}
+function fixFormulario(e) {
+    e.preventDefault();
 }
