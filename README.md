8b3da061622f999cd837be0f158679a5683a4c10
AnaBergallo <ana.bergallo@correo.ucu.edu.uy>
2021-11-14 18:45:32 -0300

    fixed - feedback

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
 <!-- npm start -->
 <!-- npm run tailwind:watch -->
 <!-- npm run uglify -->
+
+<!-- to do: 
+HACER
+- usar typescript
+- AJAX
+- pedir y guardar datos a usuario
+
+PREGUNTAS
+
+ 1. preguntar como cerrar modal haciendo clic afuera
+ 2. hay que volver a generar el min.js? como era? 
+ 3. optimizar estilso minimifcar, que mas corrijo? ya lo habia hecho... -->
--- docs/js/index.js
+++ docs/js/index.js
@@ -6,7 +6,15 @@ const form = document.getElementById('form');
 btn_open.addEventListener('click', showModal);
 btn_close.addEventListener('click', hideModal);
 
-form.addEventListener('submit', fixFormulario)
+form.addEventListener('submit', fixFormulario);
+
+//to do
+document.addEventListener('mousedown', (event) => {
+    var var_form = document.getElementById('modal');
+    if (!useRef().current.contains(event.target)){
+        hideModal();
+    }
+});
 
 function showModal() {
     modal_container.classList.add('show');
